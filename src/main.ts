117202fdd3480e0fcff10acda2ba1f476acbc76d
Y-junyoung <qq01050949256@gmail.com>
2024-02-20 16:08:44 +0900

    chore: change port 5050 to 80

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 엔트리 포인트, 의존성 주입도 여기서 시작됨.
 
-  await app.listen(5050);
+  await app.listen(80);
 }
 bootstrap();
